1059981217f03484f6cec83f3bbf619155b9ec96
minseokim <minseoalexkim@gmail.com>
2017-02-10 23:44:12 +0900

    implement BST functions

---
+++ CTCI/Chapter6MathAndLogicPuzzles/TheHeavyPill.js
@@ -0,0 +1,10 @@
+/*
+Given 20 bottles of pills, 19 bottles that have 1.0g pills and 1 bottle that has 1.1 gram pills,
+Find the HEAVY bottle by using the scale only once
+*/
+
+'use strict';
+
+const findHeavyBottle = () => {
+
+};
--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -11,7 +11,10 @@ BSTNode.prototype.insert = function(val) {
 
   const newNode = new BSTNode(val);
 
-  if (val > this.data) {
+  if (val === this.data) {
+    console.log('NODE already exists with same value!');
+    return null;
+  } else if (val > this.data) {
     this.rightChild === null ? this.rightChild = newNode : this.rightChild.insert(val);
   } else if (val <= this.data) {
     this.leftChild === null ? this.leftChild = newNode : this.leftChild.insert(val);
@@ -19,6 +22,37 @@ BSTNode.prototype.insert = function(val) {
 
   return newNode;
 };
+
+// BSTNode.prototype.put = function(root, val) {
+//   root = root || this;
+//   //no root, return a new Node with val(NEW ROOT)
+//   if (!root || !root.data) return new Node(val);
+
+//   if (root.data === val) {
+//     root.data = val;
+//   } else if (root.data < val) {
+//     root.rightChild = root.put(root.rightChild, val);
+//   } else {
+//     root.leftChild = root.put(root.leftChild, val);
+//   }
+//   return root;
+// };
+
+/*Iterative solution of GET*/
+BSTNode.prototype.get = function(val) {
+  let currentNode = this;
+  while (currentNode !== null) {
+    if (val === currentNode.data) {
+      return currentNode;
+    } else if (val > currentNode.data) {
+      currentNode = currentNode.rightChild;
+    } else {
+      currentNode = currentNode.leftChild;
+    }
+  }
+  //Doesn't exist
+  return null;
+}
 
 BSTNode.prototype.lookup = function(val) {
   if (!val) return null;
@@ -57,8 +91,48 @@ BSTNode.prototype.bfPrint = function() {
       queue.push(firstNode.rightChild);
     }
   }
-
 }
+BSTNode.prototype.findMin = function(root) {
+  //start at root, keep traversing left until theres no leftchild
+  let currentNode = root;
+
+  while (currentNode && currentNode.leftChild) {
+    currentNode = currentNode.leftChild;
+  }
+
+  return currentNode;
+};
+
+//find leftMost node, then replace it with its rightChild
+BSTNode.prototype.deleteMin = function(root) {
+  if (root.leftChild === null) return root.rightChild;
+  root.leftChild = this.deleteMin(root.leftChild);
+  return root;
+};
+
+BSTNode.prototype.delete = function(root, val) {
+  root === root || this;
+
+  // console.log('root currently :', root);
+  if (!root) return null;
+
+  if (root.data > val) {
+    root.leftChild = root.delete(root.leftChild, val);
+  } else if (root.data < val) {
+    root.rightChild = root.delete(root.rightChild, val);
+  } else {
+    if (root.leftChild === null) return root.rightChild;
+
+    if (root.rightChild === null) return root.leftChild;
+
+    let tempNode = root;
+
+    root = this.findMin(root.rightChild);
+    root.rightChild = this.deleteMin(root.rightChild);
+    root.leftChild = tempNode.leftChild;
+  }
+  return root;
+};
 
 const root = new BSTNode(6);
 root.insert(7);
@@ -66,5 +140,8 @@ root.insert(10);
 root.insert(17);
 root.insert(4);
 root.insert(5);
-
-console.log(root.lookup(6));
+// root.insert(5);
+console.log(root.delete(root, 4));
+console.log(root.lookup(4));
+// console.log(root.get(6));
+// console.log(root.get(4));
